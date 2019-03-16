5ad36befc03afb17d7b9586a09439aff48e366a6
ravi4all <ravikanttyagi.2295@gmail.com>
2019-03-16 11:01:17 +0530

    Add files via upload

---
+++ FileHandling/ReadCSV_Files.py
@@ -0,0 +1,10 @@
+import csv
+import pandas
+
+# with open('data.csv') as file:
+#     data = csv.reader(file)
+#     for row in data:
+#         print(row)
+
+df = pandas.read_csv('data.csv')
+print(df)
